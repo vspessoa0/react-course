88f9b6e0cd182d37baa3eb1a731dcefb03fe35bb
Vinicius Pessoa <100362556+vspessoa0@users.noreply.github.com>
2023-10-11 17:45:07 -0300

    Update index.js

--- firstexercise/index.js
+++ firstexercise/index.js
@@ -1,1 +1,19 @@
-ReactDOM.render(<h1>Hello everyone</h1>, document.getElementById("root"));
+ReactDOM.render(
+    <div>
+        <NavBar />
+        <MainContent />
+    </div>,
+    document.getElementById("root")
+);
+
+function NavBar() {
+    return (
+        <header>
+            <nav><p>NAVBAR</p></nav>
+        </header>
+    )
+}
+
+function MainContent() {
+    return (<h1>Hello</h1>)
+}
