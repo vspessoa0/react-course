ef97c93797dcc94e02c4879d8d0901d07609a704
Vinicius Pessoa <100362556+vspessoa0@users.noreply.github.com>
2023-10-16 14:46:39 -0300

    avançando no estudo

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import './App.css'
 
 const Header = () => {
   return (
     <header>
-      <nav>
-        <img src="/logo192.png" width="40px" />
+      <nav className='nav'>
+        <img src="/logo192.png" className='logo'/>
+        <ul className='nav-items'>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
       </nav>
       <h1>Fun facts about React</h1>
     </header>
