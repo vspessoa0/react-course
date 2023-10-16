827bbaf1db3640fd7ef94f4bf0851a780da1d4e1
Vinicius Pessoa <100362556+vspessoa0@users.noreply.github.com>
2023-10-16 14:18:28 -0300

    Update App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,33 @@
 import React from 'react'
 
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/logo192.png" width="40px" />
+      </nav>
+      <h1>Fun facts about React</h1>
+    </header>
+  )
+}
+
+const List = () => {
+  return (
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was orginally created by Jordan Walke</li>
+      <li>Has well over 100k stars on GitHub</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  )
+}
+
 const App = () => {
   return (
     <div>
-        <img src="/logo192.png" width="40px" />
-        <h1>Fun facts about React</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was orginally created by Jordan Walke</li>
-          <li>Has well over 100k stars on GitHub</li>
-          <li>Is maintained by Facebook</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
+        <Header />
+        <List />
     </div>
   )
 }
