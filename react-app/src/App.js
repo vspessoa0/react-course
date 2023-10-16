756c2629e3fa72fab29666c34673fbaf4c2a5abe
Vinicius Pessoa <100362556+vspessoa0@users.noreply.github.com>
2023-10-16 13:59:28 -0300

    update

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,7 +3,15 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-        App
+        <img src="/logo192.png" width="40px" />
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was orginally created by Jordan Walke</li>
+          <li>Has well over 100k stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
     </div>
   )
 }
